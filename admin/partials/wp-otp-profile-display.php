432fe4e2a24f5736b3da9d9875669f5c35ec2d45
Armando Lüscher <armando@noplanman.ch>
2016-10-28 19:08:32 +0200

    Redesign configuration section on profile page.

--- admin/partials/wp-otp-profile-display.php
+++ admin/partials/wp-otp-profile-display.php
@@ -10,19 +10,39 @@
 <h2>Set up WP-OTP</h2>
 <table class="form-table">
 	<tr>
-		<th>
-			<label for="wp_otp_qr_code_img"><?php _e( 'QR Code', 'wp-otp' ); ?></label><br>
-			<span class="description">Download any OTP Authenticator app on your smart phone and
-					scan this QR Code to setup WP-OTP.</span>
+		<th scope="row">
+			<?php if ( $otp_enabled ): ?>
+				<?php
+				printf(
+					'<em>%1$s</em><br><a href="%2$s" onclick = "return confirm(\'%3$s\')">%4$s</a>',
+					__( 'WP-OTP has been configured successfully.', 'wp-otp' ),
+					add_query_arg( 'wp-otp-delete', 'yes' ),
+					__( 'Are you sure you want to reconfigure WP-OTP?', 'wp-otp' ),
+					__( 'Reconfigure?', 'wp-otp' )
+				);
+				?>
+			<?php else: ?>
+				<em><?php _e( 'To activate WP-OTP, enter the One Time Password from your authenticator app and save your profile.',
+					'wp-otp' ); ?></em><br><br>
+				<label for="wp_otp_code"><?php _e( 'One Time Password', 'wp-otp' ); ?></label><br>
+				<input type="text" size="25" name="wp_otp_code" id="wp_otp_code"/>
+			<?php endif; ?>
+			<br><br>
+			<?php printf( __( 'OTP Secret:<br> %s', 'wp-otp' ), implode( ' ', str_split( $secret, 4 ) ) ); ?>
 		</th>
-		<td width="40%">
-			<img src="<?php echo $otp_qr_code_uri; ?>" id="wp_otp_qr_code_img"/><br>
-			<?php printf( __( 'OTP Secret: %s', 'wp-otp' ), implode( ' ', str_split( $secret, 4 ) ) ); ?>
+		<td>
+			<img src="<?php echo $otp_qr_code_uri; ?>"/><br>
 		</td>
 		<td>
+			<span class="description">
+				<?php _e( 'Download any OTP Authenticator app on your smart phone and scan the QR Code to activate WP-OTP.',
+					'wp-otp' ); ?>
+			</span><br><br>
+
 			<?php foreach ( $otp_apps as $otp_app ): ?>
 				<?php $app_name = esc_attr( $otp_app['name'] ); ?>
-				<a href="<?php echo $otp_app['uri']; ?>"><strong><?php echo $otp_app['name']; ?></strong><br>
+				<strong><?php echo $otp_app['name']; ?></strong><br>
+				<a href="<?php echo $otp_app['uri']; ?>" target="_blank">
 					<img src="<?php echo $otp_app['uri_logo']; ?>"
 					     alt="<?php echo $app_name; ?>"
 					     title="<?php echo $app_name; ?>"
@@ -42,38 +62,6 @@
 				<?php endforeach; ?>
 				<br><br>
 			<?php endforeach; ?>
-			<?php _e( 'Blackberry users can search and install any OTP app on their phone.', 'wp-otp' ); ?>
 		</td>
-	</tr>
-	<tr>
-		<?php if ( $otp_enabled ): ?>
-			<th>
-				<?php _e( 'WP-OTP Configured', 'wp-otp' ); ?>
-			</th>
-			<td colspan="2">
-				<?php
-				printf(
-					'%1$s <a href="%2$s" onclick = "return confirm(\'%3$s\')">%4$s</a>',
-					__( 'WP-OTP is already configured.', 'wp-otp' ),
-					add_query_arg( 'wp-otp-delete', 'yes' ),
-					'Are you sure you want to reconfigure WP-OTP?',
-					__( 'Reconfigure?', 'wp-otp' )
-				);
-				?>
-			</td>
-		<?php else: ?>
-			<th>
-				<label for="wp_otp_code"><?php _e( 'One Time Password', 'wp-otp' ); ?></label>
-			</th>
-			<td colspan="2">
-				<input type="text" size="25"
-				       value="<?php echo isset( $_POST['wp_otp_code'] ) ? $_POST['wp_otp_code'] : ''; ?>"
-				       name="wp_otp_code" id="wp_otp_code"/><br/>
-
-				<?php _e( 'Enter the One Time Password from Google Authenticator app on your smartphone <br>
-						WP-OTP will not work unless you Enter the OTP here and click on <b>Update Profile</b> button below.',
-					'wp-otp' ); ?>
-			</td>
-		<?php endif; ?>
 	</tr>
 </table>
