b6ff32e0b85fd9769e2c3dc643e773e84da06dc4
Armando Lüscher <armando@noplanman.ch>
2019-02-16 16:08:07 +0100

    Add stealth OTP mode.

--- public/class-wp-otp-public.php
+++ public/class-wp-otp-public.php
@@ -71,6 +71,28 @@ class Wp_Otp_Public {
 			return $user;
 		}
 
+		$user_meta_data = Wp_Otp_User_Meta::get_instance( $user->ID );
+
+		$otp = $this->get_otp_if_enabled( $user_meta_data );
+		if ( null === $otp ) {
+			return $user;
+		}
+		$otp_code = isset( $_POST['wp_otp_code'] ) ? $_POST['wp_otp_code'] : '';
+
+		// If this is a valid OTP code, all good!
+		if ( $this->verify_otp( $otp, $otp_code ) ) {
+			return $user;
+		}
+
+		// Check if a recovery code is being used.
+		$recovery_codes = $user_meta_data->get( 'recovery_codes' );
+		if ( array_key_exists( $otp_code, array_filter( $recovery_codes ) ) ) {
+			// Unset the recovery code that has just been used.
+			$recovery_codes[ $otp_code ] = false;
+			$user_meta_data->set( 'recovery_codes', $recovery_codes, true );
+			return $user;
+		}
+
 		/**
 		 * Filter for the OTP login form error text when an invalid code is entered.
 		 *
@@ -83,35 +105,101 @@ class Wp_Otp_Public {
 			__( '<strong>Invalid code!</strong> Please try again.', 'wp-otp' )
 		);
 
-		$user_meta_data = Wp_Otp_User_Meta::get_instance( $user->ID );
+		return new WP_Error( 'invalid_otp', $otp_invalid_code_text );
+	}
 
-		if ( $user_meta_data->get( 'enabled' ) && null !== $user_meta_data->get( 'secret' ) ) {
-			$otp_code = isset( $_POST['wp_otp_code'] ) ? $_POST['wp_otp_code'] : '';
-
-			/**
-			 * Filter for the OTP code expiration window.
-			 *
-			 * @since 0.1.0
-			 *
-			 * @param string $otp_window
-			 */
-			$otp_window = (int) apply_filters( 'wp_otp_code_expiration_window', 2 );
-
-			$otp = new TOTP( '', $user_meta_data->get( 'secret' ) );
-
-			// If this isn't a valid OTP code, check if it's a recovery code, else fail.
-			if ( ! $otp->verify( $otp_code, null, $otp_window ) ) {
-				$recovery_codes = $user_meta_data->get( 'recovery_codes' );
-				if ( array_key_exists( $otp_code, $recovery_codes ) && $recovery_codes[ $otp_code ] ) {
-					// Unset the recovery code that has just been used.
-					$recovery_codes[ $otp_code ] = false;
-					$user_meta_data->set( 'recovery_codes', $recovery_codes, true );
-				} else {
-					return new WP_Error( 'invalid_otp', $otp_invalid_code_text );
-				}
-			}
+	/**
+	 * Validation of the user login, to check if the stealth OTP was correct.
+	 *
+	 * @param string $username The username that's trying to log in.
+	 * @param string $password The password being used to log in.
+	 *
+	 * @return void
+	 */
+	public function login_form_stealth_validate( &$username, &$password ) {
+		$user = get_user_by( 'login', $username );
+		if ( ! $user ) {
+			return;
 		}
 
-		return $user;
+		$user_meta_data = Wp_Otp_User_Meta::get_instance( $user->ID );
+
+		$otp = $this->get_otp_if_enabled( $user_meta_data );
+		if ( null === $otp ) {
+			return;
+		}
+
+		// Check if no OTP code has been added.
+		if ( wp_check_password( $password, $user->user_pass, $user->ID ) ) {
+			// To prevent a login without OTP, modify password.
+			$password .= '_nootp';
+			return;
+		}
+
+		// First let's check for a valid OTP code input.
+		$otp_code = substr( $password, - 6 );
+		$tmp_pass = substr( $password, 0, - 6 );
+		if ( wp_check_password( $tmp_pass, $user->user_pass, $user->ID ) && $this->verify_otp( $otp, $otp_code ) ) {
+			$password = $tmp_pass;
+			return;
+		}
+
+		// Then check if it's a recovery code.
+		$recovery_codes = $user_meta_data->get( 'recovery_codes' );
+		foreach ( array_keys( array_filter( $recovery_codes ) ) as $recovery_code ) {
+			$otp_code = substr( $password, - strlen( $recovery_code ) );
+			if ( $otp_code !== $recovery_code ) {
+				continue;
+			}
+
+			$tmp_pass = substr( $password, 0, - strlen( $recovery_code ) );
+			if ( wp_check_password( $tmp_pass, $user->user_pass, $user->ID ) ) {
+				// Unset the recovery code that has just been used.
+				$recovery_codes[ $otp_code ] = false;
+				$user_meta_data->set( 'recovery_codes', $recovery_codes, true );
+				$password = $tmp_pass;
+				return;
+			}
+		}
+	}
+
+	/**
+	 * Get the TOTP object if applicable for this user.
+	 *
+	 * @since [unreleased]
+	 *
+	 * @param Wp_Otp_User_Meta $user_meta_data
+	 *
+	 * @return TOTP
+	 */
+	private function get_otp_if_enabled( $user_meta_data ) {
+		if ( $user_meta_data->get( 'enabled' ) && null !== $user_meta_data->get( 'secret' ) ) {
+			return new TOTP( '', $user_meta_data->get( 'secret' ) );
+		}
+
+		return null;
+	}
+
+	/**
+	 * Verify the OTP code using the passed TOTP object.
+	 *
+	 * @since [unreleased]
+	 *
+	 * @param TOTP   $otp
+	 * @param string $otp_code
+	 *
+	 * @return bool
+	 */
+	private function verify_otp( $otp, $otp_code ) {
+		/**
+		 * Filter for the OTP code expiration window.
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param string $otp_window
+		 */
+		$otp_window = (int) apply_filters( 'wp_otp_code_expiration_window', 2 );
+
+		return $otp->verify( $otp_code, null, $otp_window );
 	}
 }
